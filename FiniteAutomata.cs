2e926adde77a8fbb00d6beb1528a9e8584c7fd34
alex-chaplygin <alex_chaplygin@mail.ru>
2021-03-04 08:29:37 +0300

    Добавление документации

--- FiniteAutomata.cs
+++ FiniteAutomata.cs
@@ -4,16 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Compile
+namespace CompilerLibrary
 {
+    /// <summary>
+    /// Недетерминированный конечный автомат
+    /// </summary>
     public class FiniteAutomata
     {
+	/// <summary>
+	/// Переход из одного состояний в другое
+	/// </summary>
+        struct Change
+        {
+	    /// <summary>
+	    /// Исходное состояние автомата
+	    /// </summary>
+            public int from;
+	    /// <summary>
+	    /// Символ по которому осуществляется переход
+	    /// </summary>
+            public char symbol;
+	    /// <summary>
+	    /// Состояние, в которое должен перейти автомат
+	    /// </summary>
+            public int to;
+        }
+
+	/// <summary>
+	/// Пустой переход из одного состояний в другое (осуществляется всегда)
+	/// </summary>
+        struct EmptyChange
+        {
+	    /// <summary>
+	    /// Исходное состояние автомата
+	    /// </summary>
+            public int from;
+	    /// <summary>
+	    /// Состояние, в которое должен перейти автомат
+	    /// </summary>
+            public int to;
+        }
+	
+	/// <summary>
+	/// Список переходов автомата
+	/// </summary>
         List<Change> changes;
+	
+	/// <summary>
+	/// Список пустых(принудительных) переходов автомата
+	/// </summary>
         List<EmptyChange> emptyChanges;
+	
+	/// <summary>
+	/// Начальное состояние автомата
+	/// </summary>
         int startState;
+
+	/// <summary>
+	/// Конечные состояния автомата (при успешном распознавании автомат доходит до этих состояний)
+	/// </summary>
         int[] finalStates;
+	
+	/// <summary>
+	/// Текущие состояния автомата (их несколько при нескольких ветвях переходов0
+	/// </summary>
         List<int> states;
 
+	/// <summary>
+	/// Создается пустой автомат и устанавливается начальное состояние
+	/// </summary>
+        /// <param name="start">номер начального состояния</param>
         public FiniteAutomata(int start = 0)
         {
             startState = start;
@@ -24,10 +84,22 @@ namespace Compile
             finalStates = new int[] { };
 
         }
+
+	/// <summary>
+	/// Устанавливаются конечные состояния автомата
+	/// </summary>
+        /// <param name="states">конечные состояния</param>
         public void SetFinaleStates(int[] states)
         {
             finalStates = states;
         }
+	
+	/// <summary>
+	/// Добавляется переход
+	/// </summary>
+        /// <param name="from">начальное состояние</param>
+        /// <param name="symbol">символ по которому осуществляется переход</param>
+        /// <param name="to">конечное состояние</param>
         public void Add(int from, char symbol, int to)
         {
             Change change = new Change();
@@ -36,6 +108,12 @@ namespace Compile
             change.to = to;
             changes.Add(change);
         }
+
+	/// <summary>
+	/// Добавляется пустой(принудительный) переход
+	/// </summary>
+        /// <param name="from">начальное состояние</param>
+        /// <param name="to">конечное состояние</param>
         public void Add(int from, int to)
         {
             EmptyChange change = new EmptyChange();
@@ -43,19 +121,28 @@ namespace Compile
             change.to = to;
             emptyChanges.Add(change);
         }
+
+	/// <summary>
+	/// Символ поступает в автомат. Автомат меняет состояние.
+	/// </summary>
+        /// <param name="symbol">текущий символ</param>
         public void NewSymbol(char symbol)
         {
-           
                 for (int i = 0; i < states.Count; i++)
                 {
                    foreach (var change in emptyChanges)
-                    if (states[i] == change.from)
-                        states[i] = change.to;
+		       if (states[i] == change.from)
+			   states[i] = change.to;
                    foreach (var change in changes)
-                    if (states[i] == change.from && symbol == change.symbol)
-                        states[i] = change.to;
-            }
+		       if (states[i] == change.from && symbol == change.symbol)
+			   states[i] = change.to;
+		}
         }
+	
+	/// <summary>
+	/// Проверка окончания распознавания
+	/// </summary>
+        /// <returns>true - если успешное распознавание, false - неуспешное</returns>
         public bool Check()
         {
             for (int i = 0; i < finalStates.Length; i++)
@@ -64,22 +151,14 @@ namespace Compile
                      return true;
            return false;
         }
+	
+	/// <summary>
+	/// Сброс автомата в начальное состояние для нового распознавания
+	/// </summary>
         public void Reset()
         {
             states.Clear();
             states.Add(startState);
-        }
-        struct Change
-        {
-            public int from;
-            public char symbol;
-            public int to;
-        }
-
-        struct EmptyChange
-        {
-            public int from;
-            public int to;
         }
     }
 }
--- Pattern.cs
+++ Pattern.cs
@@ -4,19 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Compile
+namespace CompilerLibrary
 {
-    class Pattern
+    /// <summary>
+    /// Класс распознавания по шаблонам регулярных выражений
+    /// </summary>
+    public class Pattern
     {
-        FiniteAvtomata avtomat;
+	/// <summary>
+	/// Конечный автомат для распознавания
+	/// </summary>
+        FiniteAutomata avtomat;
+	
+	/// <summary>
+	/// По регулярному выражению создается конечный автомат
+	/// "abc" 0 ---a----> 1 ---b----> 2 ---c--->3
+	/// </summary>
+        /// <param name="pat">шаблон регулярного выражения</param>
         public Pattern (string pat)
         {
             avtomat = new FiniteAvtomata();
-            for (int i =0; i<pat.Length;i++)
+            for (int i =0; i < pat.Length; i++)
                 avtomat.Add(i, pat[i], i + 1);
             avtomat.SetFinaleStates(new int[] { pat.Length });
         }
 	
+	/// <summary>
+	/// Сопоставление строки с шаблоном регулярного выражения
+	/// </summary>
+        /// <param name="s">строка для распознавания</param>
+        /// <returns>true - строка соответствует шаблону, false - не соответствует</returns>
         public bool Match(string s)
         {
             avtomat.Reset();
