f0e843692fffec2ef63c9645f684eef8927adce5
alex-chaplygin <alex_chaplygin@mail.ru>
2021-03-04 08:04:11 +0300

    Добавление класса регулярных выражений

---
+++ Pattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compile
+{
+    class Pattern
+    {
+        FiniteAvtomata avtomat;
+        public Pattern (string pat)
+        {
+            avtomat = new FiniteAvtomata();
+            for (int i =0; i<pat.Length;i++)
+                avtomat.Add(i, pat[i], i + 1);
+            avtomat.SetFinaleStates(new int[] { pat.Length });
+        }
+	
+        public bool Match(string s)
+        {
+            avtomat.Reset();
+            foreach (var c in s)
+                avtomat.NewSymbol(c);
+            return avtomat.Check();
+        }
+    }
+}
